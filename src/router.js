c867c054bfaba125c0a0eed808b3f4f07a006466
Daniel Eicher <d.eicher@mailbox.org>
2022-05-07 11:29:39 +0200

    Add route page components, layout and styling

---
+++ src/router.js
@@ -0,0 +1,26 @@
+import { createRouter, createWebHistory } from "vue-router";
+
+import CoachDetail from "./pages/coaches/CoachesDetail.vue";
+import CoachesList from "./pages/coaches/CoachesList.vue";
+import CoachRegistration from "./pages/coaches/CoachRegistration.vue";
+import ContactCoach from "./pages/request/ContactCoach.vue";
+import RequestReceived from "./pages/request/RequestReceived.vue";
+import NotFound from "./pages/NotFound.vue";
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes: [
+    { path: "/", redirect: "/coaches" },
+    { path: "/coaches", component: CoachesList },
+    {
+      path: "/coaches/:id",
+      component: CoachDetail,
+      children: [{ path: "contact", component: ContactCoach }],
+    },
+    { path: "/register", component: CoachRegistration },
+    { path: "/request", component: RequestReceived },
+    { path: "/:notFound(.*)", component: NotFound },
+  ],
+});
+
+export default router;

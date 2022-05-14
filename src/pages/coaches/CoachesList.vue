1e465811701b038da64eff93dfc2d114ab8daea9
Daniel Eicher <d.eicher@mailbox.org>
2022-05-14 07:56:04 +0200

    Add coaches to vuex

--- src/pages/coaches/CoachesList.vue
+++ src/pages/coaches/CoachesList.vue
@@ -6,7 +6,9 @@
     <base-card>
       <div class="controls">
         <base-button mode="outline">Refresh</base-button>
-        <base-button link to="/register">Register as Coach</base-button>
+        <base-button v-if="!isCoach" link to="/register"
+          >Register as Coach</base-button
+        >
       </div>
       <ul v-if="hasCoaches">
         <coach-item
@@ -63,7 +65,11 @@ export default {
     hasCoaches() {
       return this.$store.getters["coaches/hasCoaches"];
     },
+    isCoach() {
+      return this.$store.getters["coaches/isCoach"];
+    },
   },
+
   methods: {
     setFilters(updatedFilters) {
       this.activeFilters = updatedFilters;

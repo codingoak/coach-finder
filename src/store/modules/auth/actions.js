44f2dc16fe0c2a1862ad65680d79251a0c41f2d6
Daniel Eicher <d.eicher@mailbox.org>
2022-05-16 20:51:28 +0200

    Add a login action and flow

--- src/store/modules/auth/actions.js
+++ src/store/modules/auth/actions.js
@@ -1,5 +1,34 @@
 export default {
-  login() {},
+  async login(context, payload) {
+    const response = await fetch(
+      "https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyCSs_86sIHMvAtu7NCSCoKbgLCRkUWvMMU",
+      {
+        method: "POST",
+        body: JSON.stringify({
+          email: payload.email,
+          password: payload.password,
+          returnSecureToken: true,
+        }),
+      }
+    );
+
+    const responseData = await response.json();
+
+    if (!response.ok) {
+      console.log(responseData);
+      const error = new Error(
+        responseData.message || "Failed to authenticate. Check your login"
+      );
+      throw error;
+    }
+
+    console.log(responseData);
+    context.commit("setUser", {
+      token: responseData.idToken,
+      userId: responseData.localId,
+      tokenExpiration: responseData.expiresIn,
+    });
+  },
   async signup(context, payload) {
     const response = await fetch(
       "https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyCSs_86sIHMvAtu7NCSCoKbgLCRkUWvMMU",
@@ -18,7 +47,7 @@ export default {
     if (!response.ok) {
       console.log(responseData);
       const error = new Error(
-        responseData.message || "Failed to authenticate. Check your login data."
+        responseData.message || "Failed to authenticate. Check your login"
       );
       throw error;
     }

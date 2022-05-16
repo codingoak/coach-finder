6a5347c2b751f2213adf97a89b4a8b987700014b
Daniel Eicher <d.eicher@mailbox.org>
2022-05-16 08:56:29 +0200

    Sending a put request to store coach data

--- src/store/modules/coaches/actions.js
+++ src/store/modules/coaches/actions.js
@@ -1,5 +1,6 @@
 export default {
-  registerCoach(context, payload) {
+  async registerCoach(context, payload) {
+    const userId = context.rootGetters.userId;
     const coachData = {
       id: context.rootGetters.userId,
       firstName: payload.first,
@@ -9,6 +10,23 @@ export default {
       areas: payload.areas,
     };
 
-    context.commit("registerCoach", coachData);
+    const response = await fetch(
+      `https://coach-finder-51930-default-rtdb.europe-west1.firebasedatabase.app/coaches${userId}.json`,
+      {
+        method: "PUT",
+        body: JSON.stringify(coachData),
+      }
+    );
+
+    // const responseData = await response.json();
+
+    if (!response.ok) {
+      // error...
+    }
+
+    context.commit("registerCoach", {
+      ...coachData,
+      id: userId,
+    });
   },
 };

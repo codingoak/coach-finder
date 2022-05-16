49306de815d9ed013cadd466bb62ac9b496193ea
Daniel Eicher <d.eicher@mailbox.org>
2022-05-16 09:57:14 +0200

    Fetching coach data (get http request)

--- src/store/modules/coaches/actions.js
+++ src/store/modules/coaches/actions.js
@@ -2,7 +2,6 @@ export default {
   async registerCoach(context, payload) {
     const userId = context.rootGetters.userId;
     const coachData = {
-      id: context.rootGetters.userId,
       firstName: payload.first,
       lastName: payload.last,
       description: payload.desc,
@@ -11,7 +10,7 @@ export default {
     };
 
     const response = await fetch(
-      `https://coach-finder-51930-default-rtdb.europe-west1.firebasedatabase.app/coaches${userId}.json`,
+      `https://coach-finder-51930-default-rtdb.europe-west1.firebasedatabase.app/coaches/${userId}.json`,
       {
         method: "PUT",
         body: JSON.stringify(coachData),
@@ -28,5 +27,31 @@ export default {
       ...coachData,
       id: userId,
     });
+  },
+  async loadCoaches(context) {
+    const response = await fetch(
+      `https://coach-finder-51930-default-rtdb.europe-west1.firebasedatabase.app/coaches.json`
+    );
+    const responseData = await response.json();
+
+    if (!response.ok) {
+      // error...
+    }
+
+    const coaches = [];
+
+    for (const key in responseData) {
+      const coach = {
+        id: key,
+        firstName: responseData[key].firstName,
+        lastName: responseData[key].lastName,
+        description: responseData[key].description,
+        hourlyRate: responseData[key].hourlyRate,
+        areas: responseData[key].areas,
+      };
+      coaches.push(coach);
+    }
+
+    context.commit("setCoaches", coaches);
   },
 };
